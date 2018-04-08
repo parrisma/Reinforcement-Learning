615392a545ee34515395b024921ddad11429cdf6
parris3142 <parris3142@hotmail.com>
2018-04-08 17:46:23 +0100

    project refector

--- TicTacToeAgent.py
+++ TicTacToeAgent.py
@@ -2,7 +2,7 @@ import random
 import logging
 from EvaluationException import EvaluationExcpetion
 from State import State
-from Agent import Agent
+from reflrn import Agent
 from Policy import Policy
 from ExplorationPlay import ExplorationPlay
 
